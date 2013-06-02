c080d5066be3a7318214b2ba0918cd65638558f5
Brian R Zaik <brian.r.zaik@gmail.com>
2013-06-02 15:33:29 -0700

    Removed sidebar from single pages

--- page.php
+++ page.php
@@ -36,7 +36,7 @@
 
 				<div id="inner-content" class="wrap clearfix">
 
-						<div id="main" class="eightcol first clearfix" role="main">
+						<div id="main" class="clearfix" role="main">
 
 							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
@@ -79,8 +79,6 @@
 							<?php endif; ?>
 
 						</div> <!-- end #main -->
-
-						<?php get_sidebar(); ?>
 
 				</div> <!-- end #inner-content -->
 
