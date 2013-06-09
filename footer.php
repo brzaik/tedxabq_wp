9a19de959474b9c143e1f971ebdfc48911d0ad17
Brian R Zaik <brian.r.zaik@gmail.com>
2013-06-08 21:05:52 -0700

    Modified footer layout

--- footer.php
+++ footer.php
@@ -2,35 +2,21 @@
 
 				<div id="inner-footer" class="wrap clearfix">
 
-					<div class="fourcol first">
+					<div class="sixcol first">
 						
 						<a href="<?php echo home_url(); ?>" rel="nofollow"><img src="<?php echo get_template_directory_uri(); ?>/library/images/tedxabq_mainlogo_white.png" /></a>
 
-						<br /><br />
+						<br />
 
 						<p class="source-org copyright"><p>This independent <a href="http://www.ted.com/tedx">TEDx</a> event is operated under license from <a href="http://ted.com">TED</a>.</p></p>
 
-						<br />
-
-						<nav role="navigation">
-							<?php bones_footer_links(); ?>
-						</nav>
-
-						<br />
-
-					</div>
-
-					<div class="fivecol">
-						
 						<h4 class="widgettitle">What is TEDx?</h4>
 						<p>In the spirit of ideas worth spreading, TED has created a program called TEDx. TEDx is a program of local, self-organized events that bring people together to share a TED-like experience. Our event is called TEDxABQ, where x = independently organized TED event.</p>
 						<p>At our TEDxABQ event, TEDTalks video and live speakers will combine to spark deep discussion and connection in a small group. The TED Conference provides general guidance for the TEDx program, but individual TEDx events, including ours, are self-organized.</p>
 
-						<br />
-
 					</div>
 
-					<div class="threecol">
+					<div class="sixcol">
 
 						<?php get_sidebar( 'footer_sidebar1' ); ?>
 
